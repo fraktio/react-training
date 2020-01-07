152e40cd61b364b4a2a4f3c158f304dcce1647f3
Mikko Hirvonen <mikko.hirvonen@fraktio.fi>
2020-01-07 07:21:00 +0200

    List persons.

--- client/src/app/components/App.tsx
+++ client/src/app/components/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
+import { persons } from '../persons'
+
 export function App() {
   return (
     <div>
       <header>
         <h1>Welcome to Fraktio's React training!</h1>
+
+        <h2>Here's your persons:</h2>
       </header>
 
-      <code>There will be code.</code>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.uuid}>
+            id: {person.uuid}, name: {person.firstName} {person.lastName}, age: {person.age}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
