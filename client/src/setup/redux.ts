d8e4698e8902557d5af39f286a3f849b85b33b1b
Mikko Hirvonen <mikko.hirvonen@fraktio.fi>
2020-01-08 18:56:22 +0200

    Ducks and sagas skeleton.

---
+++ client/src/setup/redux.ts
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import { rootReducer } from '../app/ducks'
+import { rootSaga } from '../app/sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)))
+
+sagaMiddleware.run(rootSaga)
