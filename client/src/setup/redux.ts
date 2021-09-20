e9a1a50b4380575ac39e26dca9941699bab1a106
Mikko Hirvonen <mikko.hirvonen@fraktio.fi>
2021-09-20 20:08:04 +0300

    chore: update client deps

--- client/src/setup/redux.ts
+++ client/src/setup/redux.ts
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import createSagaMiddleware from 'redux-saga'
 
 import { rootReducer } from '../app/ducks'
 import { rootSaga } from '../app/sagas'
