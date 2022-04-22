9752f451f2f33a9df01ef55241e35251c46e2dd0
Mikko Hirvonen <mikko.hirvonen@fraktio.fi>
2022-04-22 09:47:48 +0300

    list persons

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from './layout/Header'
+import { people } from './people'
 
 export function App(): JSX.Element {
   return (
@@ -6,6 +7,20 @@ export function App(): JSX.Element {
       <Header />
 
       <p>Let's build something mighty!</p>
+
+      <ul>
+        {people.map((person) => (
+          <li key={person.uuid}>
+            <p>
+              {person.firstName} {person.lastName}
+            </p>
+
+            <p>Experience: {person.experience}</p>
+
+            {person.email && <p>Email: {person.email}</p>}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
