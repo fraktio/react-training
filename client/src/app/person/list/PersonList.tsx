a9f2772762eedc2470ee6fb62b37d597413c7e71
Mikko Hirvonen <mikko.hirvonen@fraktio.fi>
2022-04-22 11:25:44 +0300

    person list

--- client/src/app/person/list/PersonList.tsx
+++ client/src/app/person/list/PersonList.tsx
@@ -1,3 +1,30 @@
-export function PersonList(): JSX.Element {
-  return <></>
+import { PersonCard, Person as PersonCardPerson } from '../card/PersonCard'
+
+import { PersonListHeader } from './PersonListHeader'
+
+type Props = {
+  people: Person[]
+}
+
+type Person = {
+  uuid: string
+} & PersonCardPerson
+
+export function PersonList({ people }: Props): JSX.Element {
+  return (
+    <div>
+      <PersonListHeader
+        title="Potential candidates"
+        description={<>Showing {people.length} people</>}
+      />
+
+      <ul>
+        {people.map((person) => (
+          <li key={person.uuid}>
+            <PersonCard person={person} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
