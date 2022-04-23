5ca68bf191db219e6a001a502f10b2c452c85dd9
Mikko Hirvonen <mikko.hirvonen@fraktio.fi>
2022-04-23 14:27:29 +0300

    use effect

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,14 +1,29 @@
 import styled from '@emotion/styled'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { DarkMode } from './DarkMode'
+import { Timer } from './Timer'
 
 export function Header(): JSX.Element {
+  const [time, setTime] = useState(0)
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setTime((previousTime) => previousTime + 1),
+      1000
+    )
+
+    return () => clearInterval(intervalId)
+  }, [])
+
   return (
     <Container>
       <Link to="/">
         <Title>Epic recruitment application</Title>
       </Link>
+
+      <Timer time={time} />
 
       <DarkMode />
     </Container>
