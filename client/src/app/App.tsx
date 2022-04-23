fe0c6d6a2c6c90dfa53b7006f7b307003ae8a9af
Mikko Hirvonen <mikko.hirvonen@fraktio.fi>
2022-04-23 15:05:09 +0300

    sort people

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react'
+
 import { Header } from './layout/Header'
+import { orderPeople } from './pages/orderPeople'
 import { people } from './people'
+import { OrderAndFilters } from './person/OrderAndFilters/OrderAndFilters'
 import { PersonList } from './person/list/PersonList'
 
+type Order = 'asc' | 'desc'
+
 export function App(): JSX.Element {
+  const [order, setOrder] = useState<Order>('asc')
+
+  const handleToggleOrder = () => {
+    setOrder(order === 'asc' ? 'desc' : 'asc')
+  }
+
   return (
     <>
       <Header />
 
-      <PersonList people={people} />
+      <OrderAndFilters order={order} onToggleOrder={handleToggleOrder} />
+
+      <PersonList people={orderPeople(people, order)} />
     </>
   )
 }
