f69274f5f36e5de0898c34e0178c71600d74411a
Mikko Hirvonen <mikko.hirvonen@fraktio.fi>
2022-04-24 14:31:00 +0300

    filter people

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import { Header } from './layout/Header'
+import { filterPeople } from './pages/IndexPage/filterPeople'
 import { orderPeople } from './pages/orderPeople'
 import { people } from './people'
 import { OrderAndFilters } from './person/OrderAndFilters/OrderAndFilters'
@@ -10,18 +11,31 @@ type Order = 'asc' | 'desc'
 
 export function App(): JSX.Element {
   const [order, setOrder] = useState<Order>('asc')
+  const [experience, setExperience] = useState(0)
+  const [name, setName] = useState('')
 
   const handleToggleOrder = () => {
     setOrder(order === 'asc' ? 'desc' : 'asc')
+  }
+
+  const handleChangeFilters = (experience: number, name: string) => {
+    setExperience(experience)
+    setName(name)
   }
 
   return (
     <>
       <Header />
 
-      <OrderAndFilters order={order} onToggleOrder={handleToggleOrder} />
+      <OrderAndFilters
+        order={order}
+        onToggleOrder={handleToggleOrder}
+        onChangeFilters={handleChangeFilters}
+      />
 
-      <PersonList people={orderPeople(people, order)} />
+      <PersonList
+        people={filterPeople(orderPeople(people, order), experience, name)}
+      />
     </>
   )
 }
--- client/src/app/person/OrderAndFilters/FiltersForm.tsx
+++ client/src/app/person/OrderAndFilters/FiltersForm.tsx
@@ -1,23 +1,37 @@
 import { useEffect } from 'react'
 import { FormProvider, useForm, UseFormReturn } from 'react-hook-form'
 
+import { ExperienceField } from './ExperienceField'
+import { NameField } from './NameField'
+
+type Props = {
+  onSubmit: (experience: number, name: string) => void
+}
+
 export type Data = {
   experience?: string
   name?: string
 }
 
-export function FiltersForm(): JSX.Element {
+export function FiltersForm({ onSubmit }: Props): JSX.Element {
   const form = useForm<Data>()
 
   const handleSubmit = (data: Data) => {
-    console.log(data)
+    onSubmit(
+      data.experience ? parseInt(data.experience, 10) : 0,
+      data.name ?? ''
+    )
   }
 
   useSubmitOnChange(form, handleSubmit)
 
   return (
     <FormProvider {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmit)}>{/* fields */}</form>
+      <form onSubmit={form.handleSubmit(handleSubmit)}>
+        <ExperienceField />
+
+        <NameField />
+      </form>
     </FormProvider>
   )
 }
